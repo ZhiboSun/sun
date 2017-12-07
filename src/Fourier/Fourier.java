1d2f2ba83e899250c4cfbc774434d4ef813ed984
ZhiboSun <359616770@qq.com>
2017-12-07 19:10:23 +0800

    修改完的fft

--- src/Fourier/Fourier.java
+++ src/Fourier/Fourier.java
@@ -1,16 +1,11 @@
 package Fourier;
-import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
-import javax.xml.bind.annotation.XmlElementDecl.GLOBAL;
 
 import FFT.*;
-import readBmpPic.readBmp;
-import readBmpPic.readBmp.MyPanel;
 
 public class Fourier {
 	public static int width = 0;//图片的宽度
@@ -66,8 +61,7 @@ public class Fourier {
 			Complex[][] gg = FFT2d.fft2d(Complexgrayg);
 			Complex[][] gb = FFT2d.fft2d(Complexgrayb);
 			
-			BufferedImage desimage = new BufferedImage(newwidth,newheight, BufferedImage.TYPE_3BYTE_BGR);
-			BufferedImage desimageILPF = new BufferedImage(newwidth,newheight, BufferedImage.TYPE_3BYTE_BGR);
+			BufferedImage desimageLPF = new BufferedImage(newwidth,newheight, BufferedImage.TYPE_3BYTE_BGR);
 			Complex rgb[] = new Complex[3];
 			int newrgb=0;
 			int ii=0;
@@ -84,19 +78,25 @@ public class Fourier {
 	                } else {  
 	                    jj = j - newheight / 2;  
 	                } 
-	                rgb = BLPF(ii,jj,gr[i][j],gg[i][j],gb[i][j]);
+	                //滤波*********************************
+	                //修改此处函数即可
+	                rgb = ILPF(ii,jj,gr[i][j],gg[i][j],gb[i][j]);
+	                
 	                int ta = 255;
 	                gr[i][j]=rgb[0];
 	                gg[i][j]=rgb[1];
 	                gb[i][j]=rgb[2];
 					newrgb =(ta << 24) | ((int)rgb[0].abs()/100 << 16)|  ((int)rgb[1].abs()/100 << 8)|(int) rgb[2].abs()/100;
-	                desimageILPF.setRGB(ii, jj, newrgb);
-					desimage.setRGB(ii, jj, newrgb);
+	                desimageLPF.setRGB(ii, jj, newrgb);
 				}
 			}
-			//writeHighQuality(desimage, "E:\\数字图象处理\\作业五\\fft1.jpg");
-			writeHighQuality(desimageILPF, "E:\\数字图象处理\\作业五\\BLPF.jpg");
-			IFFT(gr,gg,gb);
+			//输出频域滤波之后的频谱图像
+			writeHighQuality(desimageLPF, "E:\\数字图象处理\\作业五\\频谱图像.jpg");
+			
+			//输出频域滤波之后的空域图像
+			BufferedImage desifftimage = new BufferedImage(width,height, BufferedImage.TYPE_3BYTE_BGR);
+			desifftimage = IFFT(gr,gg,gb);
+			writeHighQuality(desifftimage, "E:\\数字图象处理\\作业五\\频域滤波之后的图像.jpg");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -135,30 +135,29 @@ public class Fourier {
             return false;  
         } 
 	}
-	private static void IFFT(Complex[][] gr, Complex[][] gg, Complex[][] gb) throws IOException {
+	private static BufferedImage IFFT(Complex[][] gr, Complex[][] gg, Complex[][] gb) throws IOException {
 		Complex[][] newgr = FFT2d.ifft2d(gr);
 		Complex[][] newgg = FFT2d.ifft2d(gg);
 		Complex[][] newgb = FFT2d.ifft2d(gb);
-		BufferedImage desimageILPF1 = new BufferedImage(newwidth,newheight, BufferedImage.TYPE_3BYTE_BGR);
+		BufferedImage desimage = new BufferedImage(width,height, BufferedImage.TYPE_3BYTE_BGR);
 		int newrgb=0;
-		for (int i = 0; i < newwidth; i++) {
-			for (int j = 0; j < newheight; j++) {
+		for (int i = 0; i < width; i++) {
+			for (int j = 0; j < height; j++) {
 				int ta = 255;
 				newrgb =(ta << 24) | ((int)newgr[i][j].abs() << 16)|  ((int)newgg[i][j].abs() << 8)|(int) newgb[i][j].abs();
-				desimageILPF1.setRGB(i, j, newrgb);
+				desimage.setRGB(i, j, newrgb);
 			}
 		}
-		//writeHighQuality(desimage, "E:\\数字图象处理\\作业五\\fft1.jpg");
-		writeHighQuality(desimageILPF1, "E:\\数字图象处理\\作业五\\BLPF1.jpg");
+		return desimage;
 	}
 	//几种滤波器
 	//理想低通滤波
 	public static Complex[] ILPF(int ii, int jj, Complex gr, Complex gg, Complex gb){
 		int f = 800;//截止频率
-		if(ii*ii+jj*jj>f*f){
-			gr = gg = gb = new Complex(0,0);
-		}
 		Complex rgb[] = new Complex[3];
+		if(ii*ii+jj*jj>f*f){
+			rgb[0]= rgb[1] = rgb[2] = new Complex(0,0);
+		}
 		rgb[0] = gr;
 		rgb[1] = gg;
 		rgb[2] = gb;
@@ -166,7 +165,7 @@ public class Fourier {
 	}
 	//巴特沃斯低通滤波
 	public static Complex[] BLPF(int ii, int jj, Complex gr, Complex gg, Complex gb){
-		int D0 = 80;//截止频率
+		int D0 =1000;//截止频率
 		double D = Math.sqrt(ii*ii+jj*jj);//距离
 		int n = 1;//滤波器阶数
 		double H = 1/(1+Math.pow((D/D0),2*n));
